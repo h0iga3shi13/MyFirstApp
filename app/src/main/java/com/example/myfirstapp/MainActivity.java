ee9e289ad8cfbe51057e232db98c5820cb899177
UserName <easton3.h@gmail.com>
2020-08-11 21:06:06 +0900

    BMIの計算式（下一桁で表示）

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton(android.view.View view) {
+        TextView bmiScore = (TextView)this.findViewById(R.id.bmiScore);
+        EditText heightForm = (EditText)this.findViewById(R.id.heightForm);
+        EditText weightForm = (EditText)this.findViewById(R.id.weightForm);
+
+        String strHeight, strWeight;
+        strHeight = heightForm.getText().toString();
+        strWeight = weightForm.getText().toString();
+
+        double bmi, height, weight;
+        height = Double.parseDouble(strHeight);
+        weight = Double.parseDouble(strWeight);
+
+        height /= 100;
+        bmi = weight / (height * height);
+        String msg = "BMIスコア: " + toString().format("%.1f", bmi);
+
+        bmiScore.setText(msg);
+    }
+}
